2a60903d98e27d86a7d7c9b2f12724cb721da58e
tom-ramesh <88586002+tom-ramesh@users.noreply.github.com>
2021-08-30 19:51:29 +0530

    navbar refactor

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import wordings from "./wordings";
 import "./navbar.css";
 
 const Navbar = () => {
@@ -14,16 +15,8 @@ const Navbar = () => {
 
   return (
     <div className={`nav ${show && "nav__black"}`}>
-      <img
-        className="nav__logo"
-        src="http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
-        alt="Netflix logo"
-      />
-      <img
-        className="nav__avatar"
-        src="https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture-1024x1024.jpg"
-        alt="avatar"
-      />
+      <img className="nav__logo" src={wordings.logo} alt="Netflix logo" />
+      <img className="nav__avatar" src={wordings.avatar} alt="avatar" />
     </div>
   );
 };
---
+++ src/components/Navbar/wordings.js
@@ -0,0 +1,5 @@
+export default {
+  logo: "http://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png",
+  avatar:
+    "https://writestylesonline.com/wp-content/uploads/2018/11/Three-Statistics-That-Will-Make-You-Rethink-Your-Professional-Profile-Picture-1024x1024.jpg",
+};
